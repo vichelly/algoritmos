01785cfe99f251ead1f37680f06f6e395c021199
Vitor Lucas Fujita Felício <vitor.felicio@agile.inc>
2024-02-29 15:35:44 -0300

    add

---
+++ DataStructurePy/time.py
@@ -0,0 +1,10 @@
+import time
+
+start = time.time()
+for i in range(100):
+    # Seu código aqui
+    print(i)  # Este é um exemplo, substitua pelo código que você precisa executar
+
+end = time.time()
+
+print("função tempo de execução desse for: ", end-start)
